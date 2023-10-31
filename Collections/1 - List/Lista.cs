e805b434dd3b688cef5ae65f839b36a7b7fa353c
Willian <Willian@DESKTOP-PVJKTV0>
2023-10-30 23:28:34 -0300

    Add project files.

---
+++ Collections/1 - List/Lista.cs
@@ -0,0 +1,50 @@
+﻿/*
+ 
+A classe List<T> é uma coleção genérica em C# que faz parte do namespace System.Collections.Generic. Ela é uma das 
+coleções mais comuns e amplamente utilizadas no C# para armazenar uma lista de elementos de um tipo específico.
+ 
+1º Tipo Genérico: A classe List<T> é genérica, o que significa que você precisa especificar o tipo de dados dos elementos que serão armazenados na lista quando a cria.
+
+2º Tamanho Dinâmico: A List<T> tem um tamanho dinâmico, o que significa que você pode adicionar ou remover elementos conforme necessário. O tamanho da lista é ajustado automaticamente à medida que você adiciona ou remove elementos.
+
+3º Acesso por Índice: Os elementos em uma List<T> são acessados por meio de um índice inteiro, que começa em 0. Isso permite um acesso rápido aos elementos.
+
+4º Tipo de Dados Homogêneo: Uma List<T> contém elementos do mesmo tipo de dados, pois é uma coleção genérica.
+
+5º Eficiência: As operações de acesso, adição, remoção e pesquisa em uma List<T> têm um bom desempenho devido à sua estrutura de array redimensionável.
+
+6º Métodos e Propriedades Úteis: A classe List<T> oferece muitos métodos úteis para manipular a lista, como Add, Remove, Clear, Contains, IndexOf, Sort e muito mais.
+
+ */
+
+namespace Collections.List
+{
+    public class Lista
+    {
+        public Lista()
+        {
+            ListaDeNumeros = new List<int> { 1, 2, 3 };
+
+            ListaDeNumeros.Add(4);
+            ListaDeNumeros.Add(5);
+            ListaDeNumeros.Add(6);
+        }
+
+        public List<int> ListaDeNumeros { get; set; } = new List<int>();
+
+        public void SolicitarPrimeiroNumero()
+        {
+            Console.WriteLine($"Primeiro Número: {ListaDeNumeros[0]}");
+        }
+
+        public void SolicitarTodosNumeros()
+        {
+            Console.WriteLine("Contagem Egressão:");
+
+            foreach (var numero in ListaDeNumeros)
+            {
+                Console.Write($"{numero}, ");
+            }
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+1º ArrayList: Uma coleção de objetos que cresce dinamicamente, permitindo elementos de tipos diferentes.
+List: Uma coleção genérica de elementos de um tipo específico. Esta é a versão genérica do ArrayList.
+Dictionary: Uma coleção de pares chave-valor, onde os elementos são acessados por meio de chaves únicas.
+Queue: Uma coleção de elementos onde os elementos são acessados em uma ordem específica (FIFO - First-In-First-Out).
+Stack: Uma coleção de elementos onde os elementos são acessados em uma ordem específica (LIFO - Last-In-First-Out).
+HashSet: Uma coleção que armazena elementos únicos, semelhante a um conjunto matemático.
+LinkedList: Uma lista encadeada dupla que permite inserções e remoções eficientes em qualquer posição.
+SortedSet: Um conjunto ordenado que armazena elementos únicos e os mantém em ordem.
+Queue<T>: A versão genérica da coleção Queue.
+Stack<T>: A versão genérica da coleção Stack.
+Dictionary<TKey, TValue>: A versão genérica da coleção Dictionary.
+List<T>: A versão genérica da coleção List.
+LinkedList<T>: A versão genérica da coleção LinkedList.
+SortedList<TKey, TValue>: Uma coleção ordenada de pares chave-valor.
+BitArray: Uma coleção que armazena valores booleanos como bits.
+Hashtable: Uma versão não genérica do Dictionary.
+Queue: Uma versão não genérica do Queue<T>.
+Stack: Uma versão não genérica do Stack<T>.
+SortedList: Uma versão não genérica do SortedList<TKey, TValue>. 
+ */
+
+using Collections.List;
+
+#region Lista
+
+var lista = new Lista();
+lista.SolicitarPrimeiroNumero();
+lista.SolicitarTodosNumeros();
+
+#endregion
